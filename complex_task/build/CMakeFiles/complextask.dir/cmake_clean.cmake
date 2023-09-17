6463a166faae8cf86dd7066551e9e501f0019e07
Halluzgithub <123880732+Halluzgithub@users.noreply.github.com>
2023-09-17 12:01:05 +0800

    Add files via upload

---
+++ complex_task/build/CMakeFiles/complextask.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/complextask.dir/main.cpp.o"
+  "CMakeFiles/complextask.dir/main.cpp.o.d"
+  "complextask"
+  "complextask.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/complextask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
